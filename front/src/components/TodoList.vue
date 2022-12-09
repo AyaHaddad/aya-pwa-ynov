bd98b1c4edb150184a969d0ebac75743daceeb8d
Aya haddad <aya.haddad@millameet.com>
2022-12-09 10:22:59 +0100

    add todoList + fix css indexpage

---
+++ front/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">
+      <div class="listSection">
+          <div class="listTop">
+              <h3>liste name</h3>
+              <q-icon class="listIcon" name="more_horiz" />
+          </div>
+          <div class="listItems">
+              <TodoItem/> item 1
+          </div>
+          <p class="unfilled">
+              Cette liste ne contient aucune tâche.<br>
+              Ajoutez-en une depuis la liste.
+          </p>
+          <div class="listBottom">
+              <div class="listBottom-section">
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="screen">
-
+  <div>
+  <div class="container">
+    <div class="header">
+        <div class="title">
+          <h1>Dashboard</h1>
+        </div>
+        <div class="profileIcon">
+          <q-icon name="person" />
+        </div>
+      </div>
+      <div class="personWelcome">
+        <h2>
+          Bonjour,
+          <br>
+          Tom Dupont ! 👋
+        </h2>
+      </div>
+    <div class="lists">
+        <TodoList/>
+      </div>
+    </div>
     <NavBar />
   </div>
 </template>
@@ -8,6 +27,3 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
 </script>
-
-<style lang="scss" scoped>
-</style>>
